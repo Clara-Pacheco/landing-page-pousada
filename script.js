01f698bbf7a7f7fe31a757d37853904b468cc903
Clara <clarapachecodev@gmail.com>
2024-03-03 11:15:18 -0300

    Add a click event to the hamburguer menu that will alternate between the navMenu and hamburguer menu according to the click on the menu icon

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", handleMobileMenu);
+
+function handleMobileMenu() {
+  hamburguer.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
