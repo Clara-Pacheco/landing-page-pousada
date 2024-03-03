e4215f02cc382dc9ebde3357c4ef5d874be2a25c
Clara <clarapachecodev@gmail.com>
2024-03-03 11:24:34 -0300

    Add an eventListener to the navLink using a `forEach` method

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelector(".nav-link");
+
+console.log(navLink);
 
 hamburguer.addEventListener("click", handleMobileMenu);
 
 function handleMobileMenu() {
   hamburguer.classList.toggle("active");
   navMenu.classList.toggle("active");
+}
+navLink.forEach((element) => {
+  element.addEventListener("click", closeMenu);
+});
+
+function closeMenu() {
+  hamburguer.classList.remove("active");
+  navMenu.classList.remove("active");
 }
