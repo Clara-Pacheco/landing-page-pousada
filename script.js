4be12740fcc7b240daf4e3eb9c530b74baeed13c
Clara <clarapachecodev@gmail.com>
2024-03-04 15:10:05 -0300

    Capture more elements from DOM and alter their style when the click event happens

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelector(".nav-link");
-
-console.log(navLink);
+const navLink = document.querySelectorAll(".nav-link");
+const backgroundImg = document.querySelector(".background-img");
+const h1 = document.querySelector(".text h1");
+const p = document.querySelector(".text p");
+const span = document.querySelector(".text h1 span");
+const buttonHome = document.querySelector(".text button");
 
 hamburguer.addEventListener("click", handleMobileMenu);
 
@@ -17,4 +20,26 @@ navLink.forEach((element) => {
 function closeMenu() {
   hamburguer.classList.remove("active");
   navMenu.classList.remove("active");
+}
+
+// Section Head
+
+function img(anything) {
+  backgroundImg.src = anything;
+  h1.style.color = "white";
+  span.style.color = "#CDC6EF";
+  p.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+  buttonHome.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+
+  navLink.forEach((element) => {
+    element.style.color = "white";
+  });
+
+  navLink.forEach((element) => {
+    element.style.color = "white";
+  });
+}
+function change(change) {
+  const line = document.querySelector(".image");
+  line.style.background = change;
 }
